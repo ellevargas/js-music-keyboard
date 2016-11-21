073d6d0151cd58c97903736ea401b90313a3ecd5
Elle Vargas <vargas.elle@gmail.com>
2016-11-21 15:25:55 -0800

    got keyboard working for all keys on repeat but still working on clicks

--- noise.js
+++ noise.js
@@ -1,3 +1,18 @@
-$(document).ready( function() {
-  // your code here
-});
+$(document).ready(function() {
+
+  var callback = function(note) {
+    var key = document.getElementById(note + 'Audio');
+    key.load();
+    key.play();
+  };
+
+  $('body').keypress(function() {
+      // if (event.key == 'c')
+      callback(event.key);
+  });
+
+  $('.c').click(function() {
+    callback($(this).text())
+  })
+
+})
